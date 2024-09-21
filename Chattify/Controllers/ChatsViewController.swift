24d92b87e5ffcaa6742bfba07d486099fdd20264
Maryam308 <mariam601me@gmail.com>
2024-09-21 16:58:38 +0300

    login and register forms completed

--- Chattify/Controllers/ChatsViewController.swift
+++ Chattify/Controllers/ChatsViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ChatsViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        //check if the user is signed in
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav,animated: false)
+        }
+    }
 
 }
 
